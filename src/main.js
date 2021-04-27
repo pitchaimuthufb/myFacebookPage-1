b3cda5da7579969d9ff2de6cf63f412189610546
gaurav kumar <49759621+Rgauravraj@users.noreply.github.com>
2021-04-27 08:57:49 +0530

    Update main.js
    add file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/*
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -10,3 +11,9 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+*/
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/' (req,res) => req.send(process.env));
+app.listen (port, () => console.log('server is running on port' + port));
