b97b6a0bf9a756ed35fc2de2fabd2b3488bfe9ad
gaurav kumar <49759621+Rgauravraj@users.noreply.github.com>
2021-04-27 11:23:44 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,8 +12,3 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-app.get('/' (req,res) => req.send(process.env));
-app.listen (port, () => console.log('server is running on port' + port));
