9ba3070e4e7f01d0c3a11e7d1b63d3441b1fda66
gaurav kumar <49759621+Rgauravraj@users.noreply.github.com>
2021-07-19 11:12:55 +0530

    Update rightbody.vue

--- src/components/rightbody.vue
+++ src/components/rightbody.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="right">
-	    <span class="Opretprofil">Enter Profile details please test 01 :-</span>
+	    <span class="Opretprofil">Enter Profile details please test 02 :-</span>
     <br />
 	 <span class="asap">It works quickly.</span>
 
